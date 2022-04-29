4cb51a88f45f4a9479e6fbf033372c4e461822e4
DavSladk <davsladk+github@gmail.com>
2022-04-29 04:07:20 +0200

    expanded button examples

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ var xmlDoc, parser;
 
 function loadSlide(slideNumber)
 {
-    var slide = xmlDoc.getElementsByTagName("slide")[slideNumber];
-    document.getElementsByTagName("body")[0].innerHTML = slide.getElementsByTagName("text")[0].childNodes[0].nodeValue;
+    var slideData = xmlDoc.getElementsByTagName("slide")[slideNumber];
+    var slideButtons = slideData.getElementsByTagName("button");
+
+    var slideNode = document.createElement("div");
+    var body = document.getElementsByTagName("body")[0];
+    
+    var id = document.createAttribute("id");
+    id.value = `slide${slideNumber}`;
+    slideNode.setAttributeNode(id);
+
+    createButtons(slideButtons, slideNode);
+
+    body.innerHTML = "";
+    body.appendChild(slideNode);
 }
 
-function loadXML(input)
+function loadXML()
 {
+    let input = document.getElementsByTagName("input")[0];
     let file = input.files[0];
     let reader = new FileReader();  
     reader.readAsText(file);
@@ -17,4 +30,44 @@ function loadXML(input)
       xmlDoc = parser.parseFromString(reader.result,"text/xml");
       loadSlide(0);
     };
+}
+
+function createButtons(buttons, slideNode)
+{
+    console.log("createButtons()");
+    for (let i = 0; i < buttons.length; i++)
+    {
+        var to = buttons[i].getElementsByTagName("to")[0].childNodes[0].nodeValue;
+        var value = buttons[i].getElementsByTagName("value")[0].childNodes[0].nodeValue;
+        var classes = buttons[i].getElementsByTagName("class");
+
+        var button = document.createElement("button");
+        button.innerHTML = value;
+
+        var onclick = document.createAttribute("onclick");
+        onclick.value = `loadSlide(${to})`;
+        button.setAttributeNode(onclick);
+
+        var id = document.createAttribute("id");
+        id.value = `button${i}`;
+        button.setAttributeNode(id);
+
+        addClasses(button, classes);
+
+        slideNode.appendChild(button);
+    }
+}
+
+function addClasses(element, classes)
+{
+    var classesString = "";
+
+    for (let i = 0; i < classes.length; i++)
+    {
+        classesString += " " + classes[i].childNodes[0].nodeValue;
+    }
+
+    var a = document.createAttribute("class");
+    a.value = classesString;
+    element.setAttributeNode(a);
 }
