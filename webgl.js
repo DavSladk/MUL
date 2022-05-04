7fe98f8d32322819e9ad72ac47e51e117b415d61
draberf <draberf@gmail.com>
2022-05-04 21:43:36 +0200

    Add buttons and text frame.

--- webgl.js
+++ webgl.js
@@ -1,11 +1,79 @@
+renderElems.bgImage = "bed.jpg";
+renderElems.buttons = [
+  {
+    text: "Left",
+    class: "leftBottom",
+    to: 2
+  },
+  {
+    text: "Right",
+    class: "rightBottom",
+    to: 1
+  }
+];
+renderElems.text = "Hello world!"
+
+buttonPositions = {
+  "left":             {x: 100, y: 240},
+  "right":            {x: 560, y: 240},
+  "centerBottom":     {x:   0, y:  40},
+  "leftBottom":       {x: 100, y:  40},
+  "rightBottom":      {x: 540, y:  40}
+};
+
+const buttonW = 160;
+const buttonH = 40;
+
+let imgLoaded = false;
+let end = false;
+
+let programInfo = {};
+let buffers = {};
+let texture = null;
+
+let canvas_mouseX = 320;
+let canvas_mouseY = 240;
+
 main();
 
-//
-// Start here
-//
+
+// EVENT LISTENERS
+function canvasClick(e) {
+  var hitButton = false;
+
+  // check for buttons clicked
+  for (let i = 0; i < renderElems.buttons.length; i++) {
+    const button = renderElems.buttons[i];
+    const left   = buttonPositions[button.class].x - buttonW/2;
+    const right  = buttonPositions[button.class].x + buttonW/2;
+    const bottom = buttonPositions[button.class].y - buttonH/2;
+    const top    = buttonPositions[button.class].y + buttonH/2;
+
+    if (canvas_mouseX > left && canvas_mouseX < right &&
+      canvas_mouseY > bottom && canvas_mouseY < top) {
+        alert(`Going to slide ${button.to}`);
+        loadSlide(button.to);
+    }
+  }
+
+
+  if (!hitButton) {
+    drawScene(gl, programInfo, buffers, texture);
+  }
+}
+
+function canvasHover(e) {
+  canvas_mouseX = e.clientX-10;
+  canvas_mouseY = 480-(e.clientY-10);
+  drawScene(gl, programInfo, buffers, texture);
+}
+
+
+// MAIN
 function main() {
   const canvas = document.querySelector('#glcanvas');
-  const gl = canvas.getContext('webgl');
+  canvas.addEventListener("click", canvasClick);
+  canvas.addEventListener("mousemove", canvasHover);
 
   // If we don't have a GL context, give up now
 
@@ -15,20 +83,43 @@ function main() {
   }
 
   // Vertex shader program
-
   const vsSource = `
     attribute vec4 aVertexPosition;
+    attribute vec2 aTexCoord;
+
+    varying vec2 v_texcoord;
 
     void main() {
       gl_Position = aVertexPosition;
+      v_texcoord = aTexCoord;
     }
   `;
 
   // Fragment shader program
 
   const fsSource = `
+    precision lowp float;
+
+    varying vec2 v_texcoord;
+
+    uniform int u_hl;
+    uniform int useTexture;
+    uniform vec4 u_color;
+    uniform sampler2D u_texture;
+
     void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+      vec4 color = u_color;
+      if (useTexture > 0) {
+        color = texture2D(u_texture, v_texcoord);
+      }
+      if (u_hl > 0) {
+        vec4 highlight = vec4(1.0) - color;
+        float increment =
+          5.0*clamp(abs(v_texcoord.x-0.5)-0.4, 0.0, 0.1) +
+          5.0*clamp(abs(v_texcoord.y-0.5)-0.4, 0.0, 0.1);
+        color += increment*highlight;
+      }
+      gl_FragColor = color;
     }
   `;
 
@@ -39,21 +130,29 @@ function main() {
   // Collect all the info needed to use the shader program.
   // Look up which attribute our shader program is using
   // for aVertexPosition and look up uniform locations.
-  const programInfo = {
+  programInfo = {
     program: shaderProgram,
     attribLocations: {
       vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      texCoords: gl.getAttribLocation(shaderProgram, "aTexCoord"),
     },
     uniformLocations: {
+      useTexture: gl.getUniformLocation(shaderProgram, "useTexture"),
+      highlight: gl.getUniformLocation(shaderProgram, "u_hl"),
+      uColor: gl.getUniformLocation(shaderProgram, "u_color"),
+      uTexture: gl.getUniformLocation(shaderProgram, "u_texture")
     },
   };
 
   // Here's where we call the routine that builds all the
   // objects we'll be drawing.
-  const buffers = initBuffers(gl);
+  buffers = initBuffers(gl);
+
+  // Texture buffer
+  texture = loadTexture(gl, renderElems.bgImage);
 
   // Draw the scene
-  drawScene(gl, programInfo, buffers);
+  drawScene(gl, programInfo, buffers, texture);
 }
 
 //
@@ -73,15 +172,29 @@ function initBuffers(gl) {
 
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
+
+  const textureBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, textureBuffer);
+
+  const texCoords = [
+    0.0, 0.0,
+    1.0, 0.0,
+    0.0, 1.0,
+    1.0, 1.0
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+
   return {
     position: positionBuffer,
+    textures: textureBuffer
   };
 }
 
 //
 // Draw the scene.
 //
-function drawScene(gl, programInfo, buffers) {
+function drawScene(gl, programInfo, buffers, texture) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
   gl.enable(gl.DEPTH_TEST);           // Enable depth testing
@@ -91,15 +204,18 @@ function drawScene(gl, programInfo, buffers) {
 
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  // Tell WebGL how to pull out the positions from the position
-  // buffer into the vertexPosition attribute.
-  {
+  // ==========
+  // BACKGROUND
+  // ==========
+  if (renderElems.bgImage != "") {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    const oX = -(canvas_mouseX - 320)/1280;
+    const oY = -(canvas_mouseY - 240)/960;
     const positions = [
-      1.0,  1.0,
-     -1.0,  1.0,
-      1.0, -1.0,
-     -1.0, -1.0,
+      1.5+oX,  1.5+oY,
+     -1.5+oX,  1.5+oY,
+      1.5+oX, -1.5+oY,
+     -1.5+oX, -1.5+oY,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.DYNAMIC_DRAW);
     gl.vertexAttribPointer(
@@ -107,15 +223,111 @@ function drawScene(gl, programInfo, buffers) {
         2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(
         programInfo.attribLocations.vertexPosition);
+
+    // set texture coords
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.textures);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.texCoords, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(programInfo.attribLocations.texCoords);
+      
+    gl.useProgram(programInfo.program);
+
+    {
+      const offset = 0;
+      const vertexCount = 4;
+
+      gl.activeTexture(gl.TEXTURE0);
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.uniform1i(programInfo.uniformLocations.uTexture, 0);
+      gl.uniform1i(programInfo.uniformLocations.highlight, 0);
+      gl.uniform1i(programInfo.uniformLocations.useTexture, 1);
+      
+      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    }
   }
 
-  gl.useProgram(programInfo.program);
 
-  {
-    const offset = 0;
-    const vertexCount = 4;
-    
-    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+  // =======
+  // BUTTONS
+  // =======
+  for (let i = 0; i < renderElems.buttons.length; i++) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    const button = renderElems.buttons[i];
+    const left   = buttonPositions[button.class].x - buttonW/2;
+    const right  = buttonPositions[button.class].x + buttonW/2;
+    const bottom = buttonPositions[button.class].y - buttonH/2;
+    const top    = buttonPositions[button.class].y + buttonH/2;
+    const cY = buttonPositions[button.class].y;
+    const positions = [
+      left,  bottom,
+      right, bottom,
+      left,  top,
+      right, top,
+    ];
+
+    // check for highlighting
+    var useHighlight = false;
+    if (canvas_mouseX > left && canvas_mouseX < right &&
+      canvas_mouseY > bottom && canvas_mouseY < top) {
+      useHighlight = true;
+    }
+
+    // normalize
+    for (let p = 0; p < positions.length; p+=2) {
+      positions[p] = (2*positions[p]/640)-1;
+      positions[p+1] = (2*positions[p+1]/480)-1;
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.DYNAMIC_DRAW);
+    gl.vertexAttribPointer(
+      programInfo.attribLocations.vertexPosition,
+      2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(
+      programInfo.attribLocations.vertexPosition);
+
+    gl.useProgram(programInfo.program);
+
+    {
+      const offset = 0;
+      const vertexCount = 4;
+
+      gl.uniform1i(programInfo.uniformLocations.highlight, useHighlight ? 1 : 0);
+      gl.uniform1i(programInfo.uniformLocations.useTexture, 0);
+      gl.uniform4fv(programInfo.uniformLocations.uColor, new Float32Array([0.7, 0.5, 0.05, 1.0]))
+      
+      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    }
+  }
+
+  // ====
+  // TEXT
+  // ====
+  if (renderElems.text != "") {
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    const positions = [
+      -0.95, 0.8,
+       0.95, 0.8,
+      -0.95, 0.95,
+       0.95, 0.95,
+    ]
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.DYNAMIC_DRAW);
+    gl.vertexAttribPointer(
+      programInfo.attribLocations.vertexPosition,
+      2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(
+      programInfo.attribLocations.vertexPosition);
+
+    gl.useProgram(programInfo.program);
+
+    {
+      const offset = 0;
+      const vertexCount = 4;
+
+      gl.uniform1i(programInfo.uniformLocations.highlight, 0);
+      gl.uniform1i(programInfo.uniformLocations.useTexture, 0);
+      gl.uniform4fv(programInfo.uniformLocations.uColor, new Float32Array([0.0, 0.0, 0.0, 1.0]))
+      
+      gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+    }
   }
 }
 
@@ -167,4 +379,33 @@ function loadShader(gl, type, source) {
   }
 
   return shader;
+}
+
+// load textures (called with every new slide)
+function loadTexture(gl, src) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  // enter placeholder
+  gl.texImage2D(
+    gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([255,0,0,255])
+  );
+
+  // load image
+  const image = new Image();
+  image.onload = function () {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    drawScene(gl, programInfo, buffers, texture);
+  };
+  image.crossOrigin = "";
+  if (src == "bed.jpg") {
+    image.src = "https://i.imgur.com/zdio00e.jpeg";
+  } else {
+    image.src = "https://i.imgur.com/xn7lVCw.jpeg";
+  }
+
+  return texture;
 }
